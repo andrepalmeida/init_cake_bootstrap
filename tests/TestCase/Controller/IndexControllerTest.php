a81bbd71638d3286e0d922eb60f01d2f86ff4627
Andre Almeida <andrepalmeida01@gmail.com>
2019-05-24 11:58:15 -0300

    correçao routes

--- tests/TestCase/Controller/IndexControllerTest.php
+++ tests/TestCase/Controller/IndexControllerTest.php
@@ -14,18 +14,13 @@
  */
 namespace App\Test\TestCase\Controller;
 
-use App\Controller\PagesController;
-use Cake\Core\App;
 use Cake\Core\Configure;
-use Cake\Http\Response;
-use Cake\Http\ServerRequest;
 use Cake\TestSuite\IntegrationTestCase;
-use Cake\View\Exception\MissingTemplateException;
 
 /**
- * PagesControllerTest class
+ * IndexControllerTest class
  */
-class PagesControllerTest extends IntegrationTestCase
+class IndexControllerTest extends IntegrationTestCase
 {
     /**
      * testMultipleGet method
